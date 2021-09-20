577e6beb5ed2a2de4cc53584db8efd481b10763e
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:11:29 +0300

    import global AppHeader.vue to main.js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@/assets/main.css";
+import AppHeader from "@/components/AppHeader.vue";
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
 app.config.globalProperties.$axios = axios;
-
+app.component("AppHeader", AppHeader);
 app.mount("#app");
