86a5bf0824750ab338b096398a1eed03c0950396
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:08:51 +0300

    import axios and main.css to main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@/assets/main.css";
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
 app.config.globalProperties.$axios = axios;
+
 app.mount("#app");
