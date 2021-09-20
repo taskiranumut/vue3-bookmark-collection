d8ad51e5759345d53ff8a5c1da4643c29968b9cb
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:16:19 +0300

    create NewBookmarkButton component

---
+++ src/components/NewBookmarkButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex justify-content-end mb-3">
+    <button @click="newBookmark" class="btn border">+ New</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    newBookmark() {
+      console.log("newBookmark works");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  background-color: #e9445fb3;
+  color: #e6e6e6;
+}
+
+.btn:hover {
+  background-color: #e9445f;
+  color: #e6e6e6;
+  font-weight: bold;
+}
+</style>
