1deb43402579721fefa6c9dede4d80ba73839897
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:23:53 +0300

    create BookmarkItem component

---
+++ src/components/BookmarkItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="d-flex justify-content-between border radius bg-hover px-4 mt-4"
+    :class="{ bgColor: bookmarkDetailsIsActive }"
+    @click="showDetails"
+  >
+    <BookmarkLink />
+    <BookmarkActionButtons />
+  </div>
+  <BookmarkDetails v-if="bookmarkDetailsIsActive" />
+</template>
+
+<script>
+import BookmarkDetails from "@/components/BookmarkDetails.vue";
+import BookmarkActionButtons from "@/components/BookmarkActionButtons.vue";
+import BookmarkLink from "@/components/BookmarkLink.vue";
+
+export default {
+  data() {
+    return {
+      bookmarkDetailsIsActive: false,
+    };
+  },
+  components: {
+    BookmarkDetails,
+    BookmarkActionButtons,
+    BookmarkLink,
+  },
+  methods: {
+    showDetails() {
+      this.bookmarkDetailsIsActive = !this.bookmarkDetailsIsActive;
+      console.log("showDetails works");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.radius {
+  border-radius: 5px;
+}
+
+.bg-hover:hover {
+  background-color: #0f3460;
+  cursor: pointer;
+}
+
+.bgColor {
+  background-color: #0f3460;
+}
+</style>
