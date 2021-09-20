1e7d0a81334d957e3097f86e026b3f73e2f5a41c
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:17:29 +0300

    create BookmarkLink component

---
+++ src/components/BookmarkLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex align-items-center">
+    <a
+      href="https://github.com/taskiranumut"
+      target="_blank"
+      @click="$event.stopPropagation()"
+      >Umut Taşkıran GitHub profile</a
+    >
+  </div>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #e6e6e6;
+}
+
+a:hover {
+  color: #e94560;
+}
+</style>
