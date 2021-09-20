5fc65d80e95932f99c267c0f8d75756fd46a8efe
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:23:16 +0300

    create BookmarkDetails component

---
+++ src/components/BookmarkDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="details p-2">
+    <p>
+      <span>Description: </span>Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Quibusdam ex repudiandae illo nostrum, asperiores
+      facilis aperiam et sint sit ullam!
+    </p>
+    <p class="mb-0"><span>URL: </span><i>https://github.com/taskiranumut</i></p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  font-size: 14px;
+}
+
+span {
+  color: #e94560;
+}
+
+.details {
+  margin: 0px 20px 10px 20px;
+  background-color: #0f3460;
+  border-left: 1px solid #e6e6e6;
+  border-right: 1px solid #e6e6e6;
+  border-bottom: 1px solid #e6e6e6;
+  border-radius: 0 0 5px 5px;
+}
+</style>
