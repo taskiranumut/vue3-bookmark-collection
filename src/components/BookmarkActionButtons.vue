c47640cdc14c010a1b99f6357ad5b1c130c0e3df
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:22:40 +0300

    create BookmarkActionButtons component

---
+++ src/components/BookmarkActionButtons.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="d-flex justify-content-between m-1 p-1">
+    <button @click="editBookmark" class="btn border p-0 px-1 me-2" title="Edit">
+      <span><i class="fas fa-pen fa-lg"></i></span>
+    </button>
+    <button @click="deleteBookmark" class="btn border p-0 px-1" title="Delete">
+      <span><i class="fas fa-trash-alt fa-lg"></i></span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    editBookmark(e) {
+      e.stopPropagation();
+      console.log("editBookmark works");
+    },
+    deleteBookmark(e) {
+      e.stopPropagation();
+      console.log("deleteBookmark works");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  width: 40px;
+  height: 40px;
+  color: #e9445fb3;
+}
+
+.btn:hover {
+  font-size: 125%;
+  color: #e94560;
+}
+</style>
