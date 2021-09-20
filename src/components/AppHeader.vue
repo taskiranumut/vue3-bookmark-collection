42ea46063aaf98c9427a16b43b2b2c25c0877a57
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:10:34 +0300

    create AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center bg-color p-4 mb-5 ">
+    <h1 class="my-2">Bookmark Collection</h1>
+    <h4 class="my-2">Manage your own bookmarks !</h4>
+  </div>
+</template>
+
+<style scoped>
+.bg-color {
+  background-color: #0f3460;
+}
+</style>
