c5cc955741420229c088e7b46190c7657d0d6d6f
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-20 17:25:52 +0300

    import components to Home.vue template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Home</h1>
+  <AppHeader />
+  <div class="container">
+    <div class="col-6 offset-3 border p-3">
+      <NewBookmarkButton />
+      <BookmarkItem v-for="i in 5" :key="i" />
+    </div>
+  </div>
 </template>
+
+<script>
+import NewBookmarkButton from "@/components/NewBookmarkButton.vue";
+import BookmarkItem from "@/components/BookmarkItem.vue";
+
+export default {
+  components: {
+    NewBookmarkButton,
+    BookmarkItem,
+  },
+};
+</script>
+
+<style scoped>
+.col-6 {
+  background-color: #16213e;
+}
+
+.border {
+  border-radius: 5px;
+}
+
+img {
+  border: 5px dashed #fff;
+}
+</style>
