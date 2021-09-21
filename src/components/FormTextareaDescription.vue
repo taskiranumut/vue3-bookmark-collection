cd6feda750d7b2ba04030ee6e47c94bdac705ea5
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-21 20:29:59 +0300

    create FormTextareaDescription component

---
+++ src/components/FormTextareaDescription.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-3">
+    <label class="mb-1" for="textarea-description">Description</label>
+    <textarea
+      id="textarea-description"
+      class="form-control"
+      rows="3"
+      placeholder="Description"
+    ></textarea>
+  </div>
+</template>
+
+<style scoped>
+label {
+  color: #e94560;
+}
+</style>
