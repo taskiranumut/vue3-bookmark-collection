48a278bf8291cb874c292c5b786a27462bfb5dcd
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-21 20:30:55 +0300

    create FormInputTitle component

---
+++ src/components/FormInputTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-3">
+    <label class="mb-1" for="input-title">Title</label>
+    <input
+      type="text"
+      id="input-title"
+      class="form-control"
+      placeholder="Title"
+    />
+  </div>
+</template>
+
+<style scoped>
+label {
+  color: #e94560;
+}
+</style>
