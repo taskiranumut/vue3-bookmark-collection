79f5472f5789e58091b44e7f849033f7a3a335db
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-21 20:32:38 +0300

    import components to AddBookmark and EditBookmark

--- src/views/EditBookmark.vue
+++ src/views/EditBookmark.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>EditBookmark</h1>
+  <AppHeader />
+  <div class="container">
+    <div class="col-6 offset-3 border p-3">
+      <FormInputTitle />
+      <FormInputUrl />
+      <FormTextareaDescription />
+      <FormActionButtons />
+    </div>
+  </div>
 </template>
+
+<script>
+import FormInputTitle from "@/components/FormInputTitle.vue";
+import FormInputUrl from "@/components/FormInputUrl.vue";
+import FormTextareaDescription from "@/components/FormTextareaDescription.vue";
+import FormActionButtons from "@/components/FormActionButtons.vue";
+
+export default {
+  components: {
+    FormInputTitle,
+    FormInputUrl,
+    FormTextareaDescription,
+    FormActionButtons,
+  },
+};
+</script>
+
+<style scoped>
+.col-6 {
+  background-color: #16213e;
+}
+</style>
