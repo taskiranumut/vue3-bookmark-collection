2d5fd8bf2dc7eeb9977832cfab9d8c87d4758925
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-21 20:31:09 +0300

    create FormInputUrl component

---
+++ src/components/FormInputUrl.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="my-3">
+    <label class="mb-1" for="input-url">URL</label>
+    <input type="text" id="input-url" class="form-control" placeholder="URL" />
+  </div>
+</template>
+
+<style scoped>
+label {
+  color: #e94560;
+}
+</style>
