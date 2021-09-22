cebfafb672c75697da73094d0e0e0cbbdfab1c9a
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-22 23:52:38 +0300

    add bookmark state and getters to test store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    bookmark: {
+      bookmarkTitle: "GitHub profile - Umut Taşkıran",
+      bookmarkUrl: "https://github.com/taskiranumut",
+      bookmarkDescription: "This is first description",
+    },
+  },
+  getters: {
+    _bookmarkTitle: (state) => state.bookmark.bookmarkTitle,
+    _bookmarkUrl: (state) => state.bookmark.bookmarkUrl,
+    _bookmarkDescription: (state) => state.bookmark.bookmarkDescription,
+  },
   mutations: {},
   actions: {},
   modules: {},
