78c347e244e45e87e586b0b3ac71a72120227edb
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-23 23:32:30 +0300

    call fillBookmarkList func and bookmarkList state in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,42 @@
-<template>
-  <AppHeader />
-  <div class="container">
-    <div class="col-6 offset-3 border p-3">
-      <NewBookmarkButton />
-      <BookmarkItem />
-    </div>
-  </div>
-</template>
-
 <script>
 import NewBookmarkButton from "@/components/NewBookmarkButton.vue";
 import BookmarkItem from "@/components/BookmarkItem.vue";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
     NewBookmarkButton,
     BookmarkItem,
   },
+  created() {
+    this.fillBookmarkList();
+  },
+  methods: {
+    ...mapActions({
+      fillBookmarkList: "fillBookmarkList",
+    }),
+  },
+  computed: {
+    ...mapGetters({
+      bookmarkList: "_bookmarkList",
+    }),
+  },
 };
 </script>
+
+<template>
+  <AppHeader />
+  <div class="container">
+    <div class="col-6 offset-3 border p-3">
+      <NewBookmarkButton />
+      <BookmarkItem
+        v-for="bookmark in bookmarkList"
+        :key="bookmark.id"
+        :bookmark="bookmark"
+      />
+    </div>
+  </div>
+</template>
 
 <style scoped>
 .col-6 {
