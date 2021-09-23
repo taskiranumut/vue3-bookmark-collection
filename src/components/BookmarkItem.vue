60512fd8ae5a78fe0a8f9e6780c60bb04ac98d3f
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-23 23:34:06 +0300

    add "bookmark" prop to BookmarkItem.vue

--- src/components/BookmarkItem.vue
+++ src/components/BookmarkItem.vue
@@ -1,39 +1,39 @@
-<template>
-  <div
-    class="d-flex justify-content-between border bg-hover px-4 mt-4"
-    :class="{ bgColor: bookmarkDetailsIsActive }"
-    @click="showDetails"
-  >
-    <BookmarkLink />
-    <BookmarkActionButtons />
-  </div>
-  <BookmarkDetails v-if="bookmarkDetailsIsActive" />
-</template>
-
 <script>
 import BookmarkDetails from "@/components/BookmarkDetails.vue";
 import BookmarkActionButtons from "@/components/BookmarkActionButtons.vue";
 import BookmarkLink from "@/components/BookmarkLink.vue";
 
 export default {
-  data() {
-    return {
-      bookmarkDetailsIsActive: false,
-    };
-  },
   components: {
     BookmarkDetails,
     BookmarkActionButtons,
     BookmarkLink,
   },
+  props: ["bookmark"],
+  data() {
+    return {
+      bookmarkDetailsIsActive: false,
+    };
+  },
   methods: {
     showDetails() {
       this.bookmarkDetailsIsActive = !this.bookmarkDetailsIsActive;
-      console.log("showDetails works");
     },
   },
 };
 </script>
+
+<template>
+  <div
+    class="d-flex justify-content-between border bg-hover px-4 mt-4"
+    :class="{ bgColor: bookmarkDetailsIsActive }"
+    @click="showDetails"
+  >
+    <BookmarkLink :bookmark="bookmark" />
+    <BookmarkActionButtons />
+  </div>
+  <BookmarkDetails v-if="bookmarkDetailsIsActive" :bookmark="bookmark" />
+</template>
 
 <style scoped>
 .bg-hover:hover {
