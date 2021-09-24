2c0b0c32227dd254edf96ce6820907dbd836282b
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-24 11:13:45 +0300

    add bookmarkItems mutations to mutations.js

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,4 +2,21 @@ export default {
   fillBookmarkList(state, list) {
     state.bookmarkList = list;
   },
+  addToBookmarkList(state, bookmark) {
+    state.bookmarkList.push(bookmark);
+  },
+  setBookmarkTitle(state, title) {
+    state.bookmarkItems.bookmarkTitle = title;
+  },
+  setBookmarkUrl(state, url) {
+    state.bookmarkItems.bookmarkUrl = url;
+  },
+  setBookmarkDescription(state, description) {
+    state.bookmarkItems.bookmarkDescription = description;
+  },
+  resetBookmarkItems(state) {
+    state.bookmarkItems.bookmarkTitle = "";
+    state.bookmarkItems.bookmarkUrl = "";
+    state.bookmarkItems.bookmarkDescription = "";
+  },
 };
