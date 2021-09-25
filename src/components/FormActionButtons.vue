ab047ae79ec434fe673c628acd58a6dc775d64d2
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-25 23:43:13 +0300

    refactor FormActionButtons

--- src/components/FormActionButtons.vue
+++ src/components/FormActionButtons.vue
@@ -1,83 +1,18 @@
-<template>
-  <div class="d-flex justify-content-end align-items-center p-2 px-2">
-    <button @click="saveBookmark" class="btn save-btn border me-2">Save</button>
-    <button @click="cancelOperation" class="btn cancel-btn border">
-      Cancel
-    </button>
-  </div>
-</template>
-
 <script>
-import { mapActions, mapGetters, mapMutations } from "vuex";
+import FormSaveButton from "@/components/FormSaveButton.vue";
+import FormCancelButton from "@/components/FormCancelButton.vue";
+
 export default {
-  methods: {
-    ...mapActions({
-      postBookmark: "postBookmark",
-    }),
-    ...mapMutations({
-      addToBookmarkList: "addToBookmarkList",
-    }),
-    saveBookmark() {
-      const bookmark = {
-        title: this.bookmarkTitle,
-        url: this.bookmarkUrl,
-        description: this.bookmarkDescription,
-      };
-      this.postBookmark(bookmark);
-      this.$router.push({
-        name: "Home",
-      });
-    },
-    cancelOperation() {
-      this.$router.push({
-        name: "Home",
-      });
-    },
-  },
-  computed: {
-    ...mapGetters({
-      bookmarkTitle: "_bookmarkTitle",
-      bookmarkUrl: "_bookmarkUrl",
-      bookmarkDescription: "_bookmarkDescription",
-    }),
+  components: {
+    FormSaveButton,
+    FormCancelButton,
   },
 };
 </script>
 
-<style scoped>
-.save-btn {
-  background-color: #e9445fb3;
-  color: #e6e6e6;
-  width: 76px;
-}
-
-.save-btn:hover {
-  background-color: #e9445f;
-  color: #e6e6e6;
-  font-weight: bold;
-}
-
-.save-btn:focus {
-  background-color: #e9445f;
-  color: #e6e6e6;
-  font-weight: bold;
-}
-
-.cancel-btn {
-  background-color: #0f3461b3;
-  color: #e6e6e6;
-  width: 76px;
-}
-
-.cancel-btn:hover {
-  background-color: #0f3461;
-  color: #e6e6e6;
-  font-weight: bold;
-}
-
-.cancel-btn:focus {
-  background-color: #0f3461;
-  color: #e6e6e6;
-  font-weight: bold;
-}
-</style>
+<template>
+  <div class="d-flex justify-content-end align-items-center p-2 px-2">
+    <FormSaveButton />
+    <FormCancelButton />
+  </div>
+</template>
