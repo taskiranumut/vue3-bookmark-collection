103771f2107f10eb8ad1b54887bc2a6065db37e9
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-25 23:40:37 +0300

    add "_bookmarkItems" getters to getters.js

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,6 @@
 export default {
   _bookmarkList: (state) => state.bookmarkList,
+  _bookmarkItems: (state) => state.bookmarkItems,
   _bookmarkTitle: (state) => state.bookmarkItems.bookmarkTitle,
   _bookmarkUrl: (state) => state.bookmarkItems.bookmarkUrl,
   _bookmarkDescription: (state) => state.bookmarkItems.bookmarkDescription,
