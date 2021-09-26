c6b485aaab234d4bd84b4bd7e267072d48834e7e
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-26 21:10:40 +0300

    create BookmarkEditButton component

---
+++ src/components/BookmarkEditButton.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  methods: {
+    editBookmark(e) {
+      e.stopPropagation();
+      const id = "demo123";
+      this.getRouteEditBookmark(id);
+    },
+    getRouteEditBookmark(bookmarkId) {
+      this.$router.push({
+        name: "EditBookmark",
+        params: {
+          id: bookmarkId,
+        },
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="editBookmark" class="btn border p-0 px-1 me-2" title="Edit">
+    <span><i class="fas fa-pen fa-lg"></i></span>
+  </button>
+</template>
