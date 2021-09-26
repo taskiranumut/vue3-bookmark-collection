3a391f64bbbaac869e4b8d4c54627ec2ae77b499
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-26 21:10:31 +0300

    create BookmarkDeleteButton component

---
+++ src/components/BookmarkDeleteButton.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  methods: {
+    deleteBookmark(e) {
+      e.stopPropagation();
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="deleteBookmark" class="btn border p-0 px-1" title="Delete">
+    <span><i class="fas fa-trash-alt fa-lg"></i></span>
+  </button>
+</template>
