02fb4bb77594f8540a275b2a9d03175724c37616
umuttaskiran <umut.taskiran7@gmail.com>
2021-09-26 21:10:58 +0300

    refactor BookmarkActionButtons

--- src/components/BookmarkActionButtons.vue
+++ src/components/BookmarkActionButtons.vue
@@ -1,34 +1,21 @@
-<template>
-  <div class="d-flex justify-content-between m-1 p-1">
-    <button @click="editBookmark" class="btn border p-0 px-1 me-2" title="Edit">
-      <span><i class="fas fa-pen fa-lg"></i></span>
-    </button>
-    <button @click="deleteBookmark" class="btn border p-0 px-1" title="Delete">
-      <span><i class="fas fa-trash-alt fa-lg"></i></span>
-    </button>
-  </div>
-</template>
-
 <script>
+import BookmarkDeleteButton from "@/components/BookmarkDeleteButton.vue";
+import BookmarkEditButton from "@/components/BookmarkEditButton.vue";
+
 export default {
-  methods: {
-    editBookmark(e) {
-      e.stopPropagation();
-      console.log("editBookmark works");
-      this.$router.push({
-        name: "EditBookmark",
-        params: {
-          id: "demoId123",
-        },
-      });
-    },
-    deleteBookmark(e) {
-      e.stopPropagation();
-      console.log("deleteBookmark works");
-    },
+  components: {
+    BookmarkDeleteButton,
+    BookmarkEditButton,
   },
 };
 </script>
+
+<template>
+  <div class="d-flex justify-content-between m-1 p-1">
+    <BookmarkEditButton />
+    <BookmarkDeleteButton />
+  </div>
+</template>
 
 <style scoped>
 .btn {
