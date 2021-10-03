3a2ca7984285d70e14e687a38dc15acbc667a5ff
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-04 00:58:51 +0300

    create Spinner component

---
+++ src/components/shared/Spinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <div
+      class="spinner-border size mb-3 mt-2 text-center"
+      role="status"
+      title="Loading..."
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.size {
+  width: 50px;
+  height: 50px;
+  color: #e94560;
+  font-size: 25px;
+}
+</style>
