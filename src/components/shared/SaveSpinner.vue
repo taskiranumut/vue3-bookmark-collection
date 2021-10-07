0547a765d4cfe77acab1113e54150fe7db7c204b
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-07 08:45:00 +0300

    create SaveSpinner component

---
+++ src/components/shared/SaveSpinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <div
+      class="spinner-border size text-center"
+      role="status"
+      title="Loading..."
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.size {
+  width: 24px;
+  height: 24px;
+  color: #e6e6e6;
+  font-size: 14px;
+}
+</style>
