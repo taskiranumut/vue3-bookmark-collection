d31f3c921403f8ee5f69676a07e5f219802eae9c
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-07 13:19:50 +0300

    create DeleteSpinner component

---
+++ src/components/shared/DeleteSpinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center">
+    <div
+      class="spinner-border size text-center"
+      role="status"
+      title="Loading..."
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.size {
+  width: 22px;
+  height: 22px;
+  color: #e94560;
+  font-size: 12px;
+}
+</style>
