4bc72f4e4442143f5a1659215fd61928ec67f2ec
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-08 23:46:48 +0300

    add warning elements and styles to FormInputTitle template

--- src/components/form/FormInputTitle.vue
+++ src/components/form/FormInputTitle.vue
@@ -18,6 +18,7 @@ export default {
     },
     ...mapGetters({
       bookmarkTitle: "_bookmarkTitle",
+      checkTitle: "_checkTitle",
     }),
   },
 };
@@ -26,12 +27,46 @@ export default {
 <template>
   <div class="my-3">
     <label class="form-label mb-1">Title</label>
+    <transition name="fade">
+      <div v-if="checkTitle.isActiveWarn" class="warn-msg">
+        <small>* Title cannot be empty!</small>
+      </div>
+    </transition>
     <input
       v-model="title"
       type="text"
       class="form-control"
       placeholder="Title"
-      required
+      :class="{ 'invalid-border': checkTitle.isActiveWarn }"
     />
   </div>
 </template>
+
+<style scoped>
+.invalid-border {
+  border: 1px solid #ffff00cd;
+  transition: all 0.15s;
+}
+
+.warn-msg {
+  color: #ffff00cd;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.15s ease;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-active {
+  transition: all 0.15s ease;
+}
+</style>
