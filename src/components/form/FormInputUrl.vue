631e515517f18a857cc3433e1961bedc5aa7537e
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-08 23:52:45 +0300

    add warning elements and styles to FormInputUrl template

--- src/components/form/FormInputUrl.vue
+++ src/components/form/FormInputUrl.vue
@@ -18,6 +18,7 @@ export default {
     },
     ...mapGetters({
       bookmarkUrl: "_bookmarkUrl",
+      checkUrl: "_checkUrl",
     }),
   },
 };
@@ -26,12 +27,46 @@ export default {
 <template>
   <div class="my-3">
     <label class="form-label mb-1">URL</label>
+    <transition name="fade">
+      <div v-if="checkUrl.isActiveWarn" class="warn-msg">
+        <small>* URL is invalid!</small>
+      </div>
+    </transition>
     <input
       v-model="url"
       type="text"
       class="form-control"
       placeholder="URL"
-      required
+      :class="{ 'invalid-border': checkUrl.isActiveWarn }"
     />
   </div>
 </template>
+
+<style scoped>
+.invalid-border {
+  border: 1px solid #ffff00cd;
+  transition: all 0.15s;
+}
+
+.warn-msg {
+  color: #ffff00cd;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.15s ease;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-active {
+  transition: all 0.15s ease;
+}
+</style>
