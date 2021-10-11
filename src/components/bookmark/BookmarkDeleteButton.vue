24e0390135ea58fc268711389d41bd5787000ad1
umuttaskiran <umut.taskiran7@gmail.com>
2021-10-11 20:34:13 +0300

    add confirm delete feature to BookmarkDeleteButton

--- src/components/bookmark/BookmarkDeleteButton.vue
+++ src/components/bookmark/BookmarkDeleteButton.vue
@@ -1,34 +1,48 @@
 <script>
 import DeleteSpinner from "@/components/shared/DeleteSpinner";
+import ConfirmDeleteModal from "@/components/bookmark/ConfirmDeleteModal";
 import { mapActions } from "vuex";
 
 export default {
   props: ["bookmarkId"],
   components: {
     DeleteSpinner,
+    ConfirmDeleteModal,
   },
   data() {
     return {
       isDeleteButton: false,
+      showModal: false,
     };
   },
   methods: {
-    async deleteBookmark(e) {
-      this.isDeleteButton = true;
+    startDelete(e) {
       e.stopPropagation();
+      this.showModal = true;
+    },
+    async deleteBookmark() {
+      this.showModal = false;
+      this.isDeleteButton = true;
       await this.deleteBookmarkFromApi(this.bookmarkId);
       this.isDeleteButton = false;
     },
-    ...mapActions({
-      deleteBookmarkFromApi: "deleteBookmarkFromApi",
-    }),
+    cancelDelete() {
+      this.showModal = false;
+    },
+    ...mapActions(["deleteBookmarkFromApi"]),
   },
 };
 </script>
 
 <template>
+  <ConfirmDeleteModal
+    v-if="showModal"
+    @click="$event.stopPropagation()"
+    @delete-bookmark="deleteBookmark"
+    @cancel-delete="cancelDelete"
+  />
   <button
-    @click="deleteBookmark"
+    @click="startDelete"
     class="btn action-btn border rounded-3 p-0 px-1"
     title="Delete"
   >
